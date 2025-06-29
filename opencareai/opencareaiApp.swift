29b8e9ebe8292e88858e0a0628907521187045cd
shruthi <sruthi2k6@gmail.com>
2025-06-29 09:54:32 -0700

    Initial Commit

---
+++ opencareai/opencareaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  opencareaiApp.swift
+//  opencareai
+//
+//  Created by Shruthi Sathya on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct opencareaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ opencareaiTests/opencareaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  opencareaiTests.swift
+//  opencareaiTests
+//
+//  Created by Shruthi Sathya on 6/29/25.
+//
+
+import Testing
+@testable import opencareai
+
+struct opencareaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
